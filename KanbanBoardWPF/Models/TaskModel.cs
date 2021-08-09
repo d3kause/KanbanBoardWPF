854fb562417841f5f50e7de034180775af574023
dekause <d3kause@mail.ru>
2021-08-09 19:46:02 +0700

    add base models

---
+++ KanbanBoardWPF/Models/TaskModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KanbanBoardWPF.Models
+{
+    internal class TaskModel
+    {
+        public int TaskId { get; set; }
+        public string TaskTitle { get; set; }
+        public string TaskDescription { get; set; }
+        public DateTime TaskDeadline { get; set; }
+        public TaskCategoryModel TaskCategory { get; set; }
+        public TaskPriorityModel TaskPriority { get; set; }
+    }
+}
