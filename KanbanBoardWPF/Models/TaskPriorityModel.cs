afcd3bcbe8e5394b26ff6801ec97dd2f29a14e9e
dekause <d3kause@mail.ru>
2021-08-09 19:46:02 +0700

    add base models

---
+++ KanbanBoardWPF/Models/TaskPriorityModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KanbanBoardWPF.Models
+{
+    internal class TaskPriorityModel
+    {
+        public int PriorityId { get; set; }
+        public int PriorityLevel { get; set; }
+        public string PriorityName { get; set; }
+    }
+}
